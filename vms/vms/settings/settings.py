5d0fc94f6a38b6382c57c8a0ee42844bae88c024
Farshad <farshad.ghanbari@hotmail.com>
2021-10-24 14:35:11 +0800

    restructured

--- vms/vms/settings/settings.py
+++ vms/vms/settings/settings.py
@@ -1,18 +1,12 @@
 from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7brd^o)pcf)$m^#co-+hg(*g(x=$y6__drbddwyisb+&z@y7g$'
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = 'h17b%r5!m%lg&k2pmet3fwk&u=8*(zuy6hh&tvji+4uoe)o5re'
 DEBUG = True
-
-# May need to add your machine IP here to allow external devices to connect.
 ALLOWED_HOSTS = []
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -21,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'survey.apps.SurveyConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -53,20 +48,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vms.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -82,17 +69,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
---
+++ vms/vms/settings/storage.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Boto3
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# AWS
+AWS_ACCESS_KEY_ID = 'AKIAYG6LJADSAINEVU6Y'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'uwavms'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+# A path prefix that wil be prepended to all uploads
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
